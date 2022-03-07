8d18aeddd7edb83d86398c641dac6a5d90213bff
carlos osorio <osoriocarlos43@gmail.com>
2022-03-06 20:40:11 -0500

    no message

---
+++ CRUD-MVC-5-master/model.dao/Conexion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace model.dao
+{
+    public class Conexion
+    {
+        //singleton
+        private static Conexion objConexion = null;
+        private SqlConnection con;
+
+        private Conexion()        
+        {
+            con = new SqlConnection("Data Source=CARLOSOSORIO;Initial Catalog=colegio;Integrated Security=True");
+        }
+
+        public static Conexion saberEstado()
+        {
+            if (objConexion == null)
+            {
+                objConexion = new Conexion();
+            }
+            return objConexion;
+        }
+
+        public  SqlConnection getCon()
+        {
+            return con;
+        }
+
+        public void cerrarConexion()
+        {
+            objConexion = null;
+        }
+
+    }
+}
